c6b7a87cf032f3ec8dff04733b654e9a3d089e8c
ryomanishida <nriyoma@gmail.com>
2021-10-15 15:08:13 +0000

    feat:ジャンルの編集、削除追加

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -16,12 +16,23 @@ class Admin::GenresController < ApplicationController
   end
 
   def edit
+    @genre = Genre.find(params[:id])
   end
 
   def update
+    genre = Genre.find(params[:id])
+    if genre.update(genre_params)
+      redirect_to edit_genre_path(genre.id)
+    else
+      @genre = Genre.find(params[:id])
+      render :edit
+    end
   end
 
   def destroy
+    genre = Genre.find(params[:id])
+    genre.destroy
+    redirect_to genres_path
   end
 
   private
