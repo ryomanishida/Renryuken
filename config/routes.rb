5e3496cf043ba7952ca6b7c96904b2df7d001c5a
ryomanishida <nriyoma@gmail.com>
2021-11-13 07:01:35 +0000

    feat:注文履歴

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,25 @@
 Rails.application.routes.draw do
-  root to: 'homes#top'
 
+  # 管理者
   devise_for :admins, controllers: {
     sessions: 'admins/sessions'
   }
+  namespace :admin do
+    get 'homes/top', as: '/'
+  end
+  namespace :admin do
+    resources :items
+  end
+  resources :genres, except: [:show, :new], to: 'admin/genres#'
+  namespace :admin do
+    resources :customers, only: [:index, :show, :edit]
+  end
+  get '/admin/order_details/:id', to: 'admin/order_details#update'
+  namespace :admin do
+    resources :orders, only: [:show, :update]
+  end
 
+  # 会員
   devise_for :customers, skip: :all
   devise_scope :customer do
     get '/customers/sign_up' => 'public/registrations#new'
@@ -14,18 +29,7 @@ Rails.application.routes.draw do
     delete '/customers/sign_out' => 'public/sessions#destroy'
     patch '/customers/withdraw' => 'public/customers#withdraw'
   end
-
-  # 管理者
-  namespace :admin do
-    resources :items
-  end
-  resources :genres, except: [:show, :new], to: 'admin/genres#'
-  namespace :admin do
-    resources :customers, only: [:index, :show, :edit]
-  end
-  get '/admin/order_details/:id', to: 'admin/order_details#update'
-
-  # 会員
+  root to: 'homes#top'
   resources 'items', only: [:index, :show], to: 'public/items#'
   delete '/cart_items/destroy_all', to: 'public/cart_items#destroy_all'
   resources 'cart_items', only: [:index, :create, :update, :destroy], to: 'public/cart_items#'
@@ -47,24 +51,11 @@ end
 #     resources :customers, only: [:index, :show, :edit, :update]
 #   end
 
-# # できたらやる
-#   namespace :admin do
-#     resources :orders, only: [:show, :update]
-#   end
-
 #   namespace :admin do
 #     get '/orders/:order_details/:id', to: 'admin/order_details#update'
 #   end
 
 # # 会員
 #   resources :items, only: [:index, :show], to: 'public/items#'
-
-#   resource :customers, only: [:edit, :update], to: 'public/customers#'
-#   get '/customers/my_page', to:'public/customers#show'
-#   get '/customers/unsubscribe', to:'public/customers#unsubscribe'
-
-#   get '/orders/complete', to: 'public/orders#complete'
-#   post '/orders/confirm', to: 'public/orders#confirm'
-#   resources :orders, only: [:new, :create, :index, :show], to: 'public/orders#'
 
 #   resources :addresses, except: [:show, :new], to: 'public/addresses#'
